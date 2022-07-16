d2ee1ddcd7b1e0d3fbc745c29f123a54b7445f7b
Jaures <jauresbeinajmin@gmail.com>
2022-07-16 23:49:21 +0100

    Creation function update

--- api/services/categories.services.js
+++ api/services/categories.services.js
@@ -21,11 +21,55 @@ async function createCategory(params, callback) {
         });
 }
 
-async function getCategory(params, callback) {
+async function getCategories(params, callback) {
     const categoryName = params.categoryName;
     var condiction = categoryName?{
         categoryName: {$regex: new RegExp(categoryName),$options:"i"}}
         :{};
-        let perPage = Math.abs(params.pageSize) || MONGO_DB_CONFIG.pageSize
+        let perPage = Math.abs(params.pageSize) || MONGO_DB_CONFIG.pageSize;
+        let page = (Math.abs(params.page) || 1) -1
+
+        category
+        .find(condition,"categoryName categoryImage")
+        .limit(perPage)
+        .skip(perPage * page)
+        .then((response) => {
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+
+    
+}
+
+
+async function getCategoryById(params, callback) {
+    const categoryId = params.categoryName;
+        category
+        .findById(categoryId)
+        .then((response) => {
+            if (!response) callback("Not Found Category", +categoryId);
+            else callback("Category: " + response)
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+
+    
+}
+
+async function updateCategory(params, callback) {
+    const categoryId = params.categoryId;
+        category
+        .findByIdAndUpdate({ categoryId , params, {useFindAndModify:false})
+        .then((response) => {
+            if (!response) callback("Not Found Category", +categoryId);
+            else callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+
     
 }
